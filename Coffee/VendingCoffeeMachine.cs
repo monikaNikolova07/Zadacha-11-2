0f0809f7e777f6a2daef3a72a3fff057d4702517
monikaNikolova07 <monica06072007@gmail.com>
2025-02-11 10:58:20 +0200

    Zadacha za unit testing ot 11.02

---
+++ Coffee/VendingCoffeeMachine.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee
+{
+    public class VendingCoffeeMachine
+    {
+        private double coffeePrice;
+        private double coffeeWithMilkPrice;
+        private double coffeeAmount;
+        private double milkAmount;
+        public double CoffeePrice
+        {
+            get { return coffeePrice; }
+            private set { coffeePrice = value; }
+        }
+
+        public double CoffeeWithMilkPrice
+        {
+            get { return coffeeWithMilkPrice; }
+            private set { coffeeWithMilkPrice = value; }
+        }
+
+        public double CoffeeAmount
+        {
+            get { return coffeeAmount; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Количество кафе не може да бъде отрицателно.");
+                }
+                coffeeAmount = value;
+            }
+        }
+
+        public double MilkAmount
+        {
+            get { return milkAmount; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Количество мляко не може да бъде отрицателно.");
+                }
+                milkAmount = value;
+            }
+        }
+        public VendingCoffeeMachine(double initialCoffeeAmount, double initialMilkAmount)
+        {
+            CoffeePrice = 1.50;
+            CoffeeWithMilkPrice = 2.00;
+            CoffeeAmount = initialCoffeeAmount;
+            MilkAmount = initialMilkAmount;
+        }
+
+        public string OrderCoffee(int choice, double insertedAmount)
+        {
+            double price;
+            if (choice == 1)
+            {
+                price = coffeePrice;
+                if (CoffeeAmount < 10)
+                {
+                    throw new InvalidOperationException("Недостатъчно кафе за приготвяне на напитката.");
+                }
+            }
+            else if (choice == 2)
+            {
+                price = CoffeeWithMilkPrice;
+                if (coffeeAmount < 10)
+                {
+                    throw new InvalidOperationException("Недостатъчно кафе за приготвяне на напитката.");
+                }
+                if (MilkAmount < 50)
+                {
+                    throw new InvalidOperationException("Недостатъчно мляко за приготвяне на напитката.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Невалиден избор. Моля, изберете 1 за кафе или 2 за кафе с мляко.");
+            }
+
+
+            if (insertedAmount < price)
+            {
+                double deficit = price - insertedAmount;
+                throw new ArgumentException($"Недостатъчно средства. Напитката струва {price:F2} лв, а вие сте въвели {insertedAmount:F2} лв. Трябват още {deficit:F2} лв.");
+            }
+
+
+            CoffeeAmount -= 10;
+            if (choice == 2)
+            {
+                MilkAmount -= 50;
+            }
+
+            double change = insertedAmount - price;
+            return $"Напитката е приготвена. Вашето ресто: {change:F2} лв.";
+        }
+
+        public double GetRemainingCoffee()
+        {
+            return CoffeeAmount;
+        }
+
+        public double GetRemainingMilk()
+        {
+            return MilkAmount;
+        }
+    
+    }
+}
---
+++ TestProject1CoffeeMashine/UnitTest1.cs
@@ -0,0 +1,129 @@
+﻿using Coffee;
+using System.Formats.Asn1;
+using System.Reflection;
+
+namespace TestProject1CoffeeMashine
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestConstruction()
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(3.0, 2.0);
+
+            Assert.That(vending.CoffeePrice == 1.50);
+            Assert.That(vending.CoffeeWithMilkPrice == 2.00);
+            Assert.That(vending.CoffeeAmount == 3); 
+            Assert.That(vending.MilkAmount == 2);
+        }
+
+        [Test]
+        public void TestCoffeePrice()
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(3.0, 2.0);
+            Assert.That(vending.CoffeePrice == 1.50);
+        }
+
+        [Test]
+        public void TestCoffeeWithMilkPrice()
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(3.0, 2.0);
+            Assert.That(vending.CoffeeWithMilkPrice == 2.00);
+        }
+
+        [Test]
+        public void TestCoffeeAmount()
+        {
+            Assert.That(() => new VendingCoffeeMachine(-4, 2.0), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void TestMilkAmount()
+        {
+            Assert.That(() => new VendingCoffeeMachine(3.0, -2.0), Throws.ArgumentException);
+        }
+
+        // Valid cases
+        [TestCase(1, 2.0, 0.5)]
+        [TestCase(2, 3.0, 1)]
+        public void OrderCoffee_ShouldOrderCorrectly(int choise, double insertedAmmount, double change)
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(12.0, 100.0);
+            Assert.That(vending.OrderCoffee(choise, insertedAmmount) == $"Напитката е приготвена. Вашето ресто: {change:F2} лв.");
+        }
+
+        // Invalid choice
+        [TestCase(0, 2.0)]
+        [TestCase(3, 3.0)]
+        public void OrderCoffee_ShouldThrow_whenInvlaidChoise(int choise, double insertedAmmount)
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(15.0, 101.0);
+            Assert.That(() => vending.OrderCoffee(choise, insertedAmmount), Throws.ArgumentException);
+        }
+
+        // Invalid cases coffee amount
+        [TestCase(1, 2.0, 0.5, 1)]
+        [TestCase(2, 3.0, 1, 1)]
+        public void OrderCoffee_ShouldDecreaseCoffeeAmountCorrectly(int choise, double insertedAmmount, double change,double asseptedAmmount)
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(11.0, 101.0);
+            vending.OrderCoffee(choise, insertedAmmount);
+            Assert.That(() => vending.CoffeeAmount == asseptedAmmount);
+        }
+
+        // Invalid choice
+        [TestCase(1, 2.0)]
+        [TestCase(2, 3.0)]
+        public void OrderCoffee_ShouldThrow_WhenInsufficientCoffee(int choise, double insertedAmmount)
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(3.0, 101.0);
+            Assert.That(() => vending.OrderCoffee(choise, insertedAmmount), Throws.InvalidOperationException);
+        }
+
+        // Valid cases
+        [TestCase(1, 2.0, 0.5)]
+        [TestCase(2, 3.0, 1)]
+        public void OrderCoffee_ShouldCalculateChangeCorrectly(int choise, double insertedAmmount, double change)
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(11.0, 101.0);
+            Assert.That(() => vending.OrderCoffee(choise, insertedAmmount) == $"Напитката е приготвена. Вашето ресто: {change:F2} лв.");
+        }
+
+        // Valid cases
+        [TestCase(2, 2.0, 0.5, 51)]
+        [TestCase(2, 3.0, 1, 51)]
+        public void OrderCoffee_ShouldDecreaseMilkAmountCorrectly(int choise, double insertedAmmount,
+            double change, double asseptedAmmount)
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(11.0, 101.0);
+            vending.OrderCoffee(choise, insertedAmmount);
+            Assert.That(() => vending.MilkAmount == asseptedAmmount);
+        }
+        
+        // Invalid choice
+        [TestCase(2, 2.0)]
+        [TestCase(2, 3.0)]
+        public void OrderCoffee_ShouldThrow_WhenInsufficientMilk(int choise, double insertedAmmount)
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(11.0, 1.0);
+            Assert.That(() => vending.OrderCoffee(choise, insertedAmmount), Throws.InvalidOperationException);
+        }
+
+        // Invalid choice
+        [TestCase(1, 0.90, 2)]
+        [TestCase(2, 0.05, 0.0)]
+        public void OrderCoffee_ShouldThrow_WhenInsufficientPrice(int choise, double insertedAmmount, double change)
+        {
+            VendingCoffeeMachine vending = new VendingCoffeeMachine(11.0, 101.0);
+            Assert.That(() => vending.OrderCoffee(choise, insertedAmmount), Throws.ArgumentException);
+        }
+
+
+    }
+}
